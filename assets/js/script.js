791ca4d5a02641b1719f2efa3ed847f031e280c3
Arif Momin <arifmomin2743@gmail.com>
2024-10-09 03:17:12 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+// #INSIDE OUR APP -> Slider
+// Swiper Slider
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+
+// #WHAT OUR CLIENTS SAY’S Slider
+const swiperslider = new Swiper(".client_says_slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+
+
